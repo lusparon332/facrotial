b3e5ee248e54d551ee6e12f0ee200b865753dfdc
lusparon332 <lusparon222@gmail.com>
2022-12-30 15:29:40 +0300

    Make project

---
+++ Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            Console.WriteLine($"Factorial of {n} is {factorial(n)}");
+        }
+        static int factorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+            return (n * factorial(n - 1));
+        }
+    }
+}
